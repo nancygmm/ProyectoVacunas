ad859ae66ee2b899c9ba5f76971b00cacd87c727
nancygmm <99931623+nancygmm@users.noreply.github.com>
2022-10-17 08:27:36 -0600

    Trabajo 17/10

---
+++ Control.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Control {
+    
+    ArrayList <Vacunas> listas = new ArrayList<Vacunas>();
+    ArrayList <Vacunas> faltan = new ArrayList<Vacunas>();
+}
--- Persona.java
+++ Persona.java
@@ -1,3 +1,47 @@
 public class Persona {
     
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int telefono;
+    private int dpi;
+
+    public Persona(String nombre, String apellido, int edad, int telefono, int dpi) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.dpi = dpi;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getDpi() {
+        return dpi;
+    }
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
 }
